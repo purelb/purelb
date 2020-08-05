07db07747d964bfb8d5043d11b4600a03bb27075
toby cabot <toby@acnodal.io>
2020-08-05 18:57:53 -0400

    Remove multiarch
    If someone asks us to build binaries for an s390 we'll see...

--- tasks.py
+++ tasks.py
@@ -1,13 +1,6 @@
 import os
 import semver
-import shutil
 import sys
-import yaml
-import tempfile
-try:
-    from urllib.request import urlopen
-except ImportError:
-    from urllib2 import urlopen
 
 from invoke import run, task
 from invoke.exceptions import Exit
@@ -16,26 +9,7 @@ all_binaries = set(["controller-pool",
                     "controller-acnodal",
                     "speaker-acnodal",
                     "speaker-local"])
-all_architectures = set(["amd64",
-                         "arm",
-                         "arm64",
-                         "ppc64le",
-                         "s390x"])
 
-def _check_architectures(architectures):
-    out = set()
-    for arch in architectures:
-        if arch == "all":
-            out |= all_architectures
-        elif arch not in all_architectures:
-            print("unknown architecture {}".format(arch))
-            print("Supported architectures: {}".format(", ".join(sorted(all_architectures))))
-            sys.exit(1)
-        else:
-            out.add(arch)
-    if not out:
-        out.add("amd64")
-    return list(sorted(out))
 
 def _check_binaries(binaries):
     out = set()
@@ -52,105 +26,65 @@ def _check_binaries(binaries):
         out |= all_binaries
     return list(sorted(out))
 
-def _make_build_dirs():
-    for arch in all_architectures:
-        for binary in all_binaries:
-            dir = os.path.join("build", arch, binary)
-            if not os.path.exists(dir):
-                os.makedirs(dir, mode=0o750)
-
-@task(iterable=["binaries", "architectures"],
-      help={
-          "binaries": "binaries to build. One or more of {}, or 'all'".format(", ".join(sorted(all_binaries))),
-          "architectures": "architectures to build. One or more of {}, or 'all'".format(", ".join(sorted(all_architectures))),
-          "tag": "docker image tag prefix to use. Actual tag will be <tag>-<arch>. Default 'dev'.",
-          "docker-user": "docker user under which to tag the images. Default 'metallb'.",
-      })
-def build(ctx, binaries, architectures, tag="dev", docker_user="metallb"):
-    """Build MetalLB docker images."""
-    binaries = _check_binaries(binaries)
-    architectures = _check_architectures(architectures)
-    _make_build_dirs()
-
-    commit = run("git describe --dirty --always", hide=True).stdout.strip()
-    branch = run("git rev-parse --abbrev-ref HEAD", hide=True).stdout.strip()
-
-    for arch in architectures:
-        env = {
-            "CGO_ENABLED": "0",
-            "GOOS": "linux",
-            "GOARCH": arch,
-            "GOARM": "6",
-            "GO111MODULE": "on",
-        }
-        for bin in binaries:
-            run("go build -v -o build/{arch}/{bin}/{bin} -ldflags "
-                "'-X go.universe.tf/metallb/internal/version.gitCommit={commit} "
-                "-X go.universe.tf/metallb/internal/version.gitBranch={branch}' "
-                "./cmd/{bin}/".format(
-                    arch=arch,
-                    bin=bin,
-                    commit=commit,
-                    branch=branch),
-                env=env,
-                echo=True)
-            run("docker build "
-                "-t {user}/{bin}:{tag}-{arch} "
-                "-f build/package/Dockerfile.{bin} build/{arch}/{bin}".format(
-                    user=docker_user,
-                    bin=bin,
-                    tag=tag,
-                    arch=arch),
-                echo=True)
-
-@task(iterable=["binaries", "architectures"],
-      help={
-          "binaries": "binaries to build. One or more of {}, or 'all'".format(", ".join(sorted(all_binaries))),
-          "architectures": "architectures to build. One or more of {}, or 'all'".format(", ".join(sorted(all_architectures))),
-          "tag": "docker image tag prefix to use. Actual tag will be <tag>-<arch>. Default 'dev'.",
-          "docker-user": "docker user under which to tag the images. Default 'metallb'.",
-      })
-def push(ctx, binaries, architectures, tag="dev", docker_user="metallb"):
-    """Build and push docker images to registry."""
-    binaries = _check_binaries(binaries)
-    architectures = _check_architectures(architectures)
-
-    for arch in architectures:
-        for bin in binaries:
-            build(ctx, binaries=[bin], architectures=[arch], tag=tag, docker_user=docker_user)
-            run("docker push {user}/{bin}:{tag}-{arch}".format(
-                user=docker_user,
-                bin=bin,
-                arch=arch,
-                tag=tag),
-                echo=True)
 
 @task(iterable=["binaries"],
       help={
           "binaries": "binaries to build. One or more of {}, or 'all'".format(", ".join(sorted(all_binaries))),
-          "tag": "docker image tag prefix to use. Actual tag will be <tag>-<arch>. Default 'dev'.",
+          "tag": "docker image tag prefix to use. Default 'dev'.",
           "docker-user": "docker user under which to tag the images. Default 'metallb'.",
       })
-def push_multiarch(ctx, binaries, tag="dev", docker_user="metallb"):
-    """Build and push multi-architecture docker images to registry."""
+def build(ctx, binaries, tag="dev", docker_user="metallb"):
+    """Build MetalLB docker images."""
     binaries = _check_binaries(binaries)
-    architectures = _check_architectures(["all"])
-    push(ctx, binaries=binaries, architectures=architectures, tag=tag, docker_user=docker_user)
 
-    platforms = ",".join("linux/{}".format(arch) for arch in architectures)
+    commit = run("git describe --dirty --always", hide=True).stdout.strip()
+    branch = run("git rev-parse --abbrev-ref HEAD", hide=True).stdout.strip()
+
+    env = {
+        "CGO_ENABLED": "0",
+        "GOOS": "linux",
+        "GOARM": "6",
+        "GO111MODULE": "on",
+    }
     for bin in binaries:
-        run("manifest-tool push from-args "
-            "--platforms {platforms} "
-            "--template {user}/{bin}:{tag}-ARCH "
-            "--target {user}/{bin}:{tag}".format(
-                platforms=platforms,
+        run("go build -v -o bin/{bin} -ldflags "
+            "'-X go.universe.tf/metallb/internal/version.gitCommit={commit} "
+            "-X go.universe.tf/metallb/internal/version.gitBranch={branch}' "
+            "./cmd/{bin}/".format(
+                bin=bin,
+                commit=commit,
+                branch=branch),
+            env=env,
+            echo=True)
+        run("docker build "
+            "-t {user}/{bin}:{tag} "
+            "-f build/package/Dockerfile.{bin} bin/".format(
                 user=docker_user,
                 bin=bin,
                 tag=tag),
             echo=True)
 
+
+@task(iterable=["binaries"],
+      help={
+          "binaries": "binaries to build. One or more of {}, or 'all'".format(", ".join(sorted(all_binaries))),
+          "tag": "docker image tag prefix to use. Default 'dev'.",
+          "docker-user": "docker user under which to tag the images. Default 'metallb'.",
+      })
+def push(ctx, binaries, tag="dev", docker_user="metallb"):
+    """Build and push docker images to registry."""
+    binaries = _check_binaries(binaries)
+
+    for bin in binaries:
+        build(ctx, binaries=[bin], tag=tag, docker_user=docker_user)
+        run("docker push {user}/{bin}:{tag}".format(
+            user=docker_user,
+            bin=bin,
+            tag=tag),
+            echo=True)
+
+
 @task(help={
-    "architecture": "CPU architecture of the local machine. Default 'amd64'.",
     "name": "name of the kind cluster to use.",
 })
 def release(ctx, version, skip_release_notes=False):
