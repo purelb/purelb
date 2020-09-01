fd5e632deee08df59973a70e169561e334f74b8f
toby cabot <toby@acnodal.io>
2020-08-31 21:04:54 -0400

    Update k8s.go to remove unused code and look more like the k8s sample controller
    There were some "if" checks for conditions that can never happen.
    Also, clientcmd.BuildConfigFromFlags can be used inside and outside of
    the cluster so we don't need to have two cases.
    
    It will likely be helpful when newcomers look at the code if it more
    closely resembles the k8s sample controller.

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package config // import "purelb.io/internal/config"
+package config
 
 import (
 	"errors"
--- internal/k8s/k8s.go
+++ internal/k8s/k8s.go
@@ -1,4 +1,4 @@
-package k8s // import "purelb.io/internal/k8s"
+package k8s
 
 import (
 	"context"
@@ -9,12 +9,12 @@ import (
 	"purelb.io/internal/config"
 
 	"github.com/go-kit/kit/log"
-	"k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
-	v1core "k8s.io/client-go/kubernetes/typed/core/v1"
+	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
@@ -42,18 +42,18 @@ type Client struct {
 
 	syncFuncs []cache.InformerSynced
 
-	serviceChanged func(log.Logger, string, *v1.Service, *v1.Endpoints) SyncState
+	serviceChanged func(log.Logger, string, *corev1.Service, *corev1.Endpoints) SyncState
 	configChanged  func(log.Logger, *config.Config) SyncState
-	nodeChanged    func(log.Logger, *v1.Node) SyncState
+	nodeChanged    func(log.Logger, *corev1.Node) SyncState
 	synced         func(log.Logger)
 }
 
 // Service offers methods to mutate a Kubernetes service object.
 type Service interface {
-	Update(svc *v1.Service) (*v1.Service, error)
-	UpdateStatus(svc *v1.Service) error
-	Infof(svc *v1.Service, desc, msg string, args ...interface{})
-	Errorf(svc *v1.Service, desc, msg string, args ...interface{})
+	Update(svc *corev1.Service) (*corev1.Service, error)
+	UpdateStatus(svc *corev1.Service) error
+	Infof(svc *corev1.Service, desc, msg string, args ...interface{})
+	Errorf(svc *corev1.Service, desc, msg string, args ...interface{})
 }
 
 // SyncState is the result of calling synchronization callbacks.
@@ -81,9 +81,9 @@ type Config struct {
 	Logger        log.Logger
 	Kubeconfig    string
 
-	ServiceChanged func(log.Logger, string, *v1.Service, *v1.Endpoints) SyncState
+	ServiceChanged func(log.Logger, string, *corev1.Service, *corev1.Endpoints) SyncState
 	ConfigChanged  func(log.Logger, *config.Config) SyncState
-	NodeChanged    func(log.Logger, *v1.Node) SyncState
+	NodeChanged    func(log.Logger, *corev1.Node) SyncState
 	Synced         func(log.Logger)
 }
 
@@ -102,15 +102,7 @@ func New(cfg *Config) (*Client, error) {
 		err       error
 	)
 
-	if cfg.Kubeconfig == "" {
-		// if the user didn't provide a config file, assume that we're
-		// running inside k8s.
-		k8sConfig, err = rest.InClusterConfig()
-	} else {
-		// the user provided a config file, so use that.  InClusterConfig
-		// would also work in this case but it emits an annoying warning.
-		k8sConfig, err = clientcmd.BuildConfigFromFlags("", cfg.Kubeconfig)
-	}
+	k8sConfig, err = clientcmd.BuildConfigFromFlags("", cfg.Kubeconfig)
 	if err != nil {
 		return nil, fmt.Errorf("building client config: %s", err)
 	}
@@ -119,18 +111,9 @@ func New(cfg *Config) (*Client, error) {
 		return nil, fmt.Errorf("creating Kubernetes client: %s", err)
 	}
 
-	namespace := cfg.ConfigMapNS
-	if namespace == "" {
-		bs, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
-		if err != nil {
-			return nil, fmt.Errorf("getting namespace from pod service account data: %s", err)
-		}
-		namespace = string(bs)
-	}
-
-	broadcaster := record.NewBroadcaster()
-	broadcaster.StartRecordingToSink(&v1core.EventSinkImpl{Interface: v1core.New(clientset.CoreV1().RESTClient()).Events("")})
-	recorder := broadcaster.NewRecorder(scheme.Scheme, v1.EventSource{Component: cfg.ProcessName})
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartRecordingToSink(&typedcorev1.EventSinkImpl{Interface: typedcorev1.New(clientset.CoreV1().RESTClient()).Events("")})
+	recorder := eventBroadcaster.NewRecorder(scheme.Scheme, corev1.EventSource{Component: cfg.ProcessName})
 
 	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
 
@@ -141,88 +124,100 @@ func New(cfg *Config) (*Client, error) {
 		queue:  queue,
 	}
 
-	if cfg.ServiceChanged != nil {
-		svcHandlers := cache.ResourceEventHandlerFuncs{
-			AddFunc: func(obj interface{}) {
-				key, err := cache.MetaNamespaceKeyFunc(obj)
-				if err == nil {
-					c.queue.Add(svcKey(key))
-				}
-			},
-			UpdateFunc: func(old interface{}, new interface{}) {
-				key, err := cache.MetaNamespaceKeyFunc(new)
-				if err == nil {
-					c.queue.Add(svcKey(key))
-				}
-			},
-			DeleteFunc: func(obj interface{}) {
-				key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
-				if err == nil {
-					c.queue.Add(svcKey(key))
-				}
-			},
-		}
-		svcWatcher := cache.NewListWatchFromClient(c.client.CoreV1().RESTClient(), "services", v1.NamespaceAll, fields.Everything())
-		c.svcIndexer, c.svcInformer = cache.NewIndexerInformer(svcWatcher, &v1.Service{}, 0, svcHandlers, cache.Indexers{})
+	// Service Watcher
 
-		c.serviceChanged = cfg.ServiceChanged
-		c.syncFuncs = append(c.syncFuncs, c.svcInformer.HasSynced)
-
-		if cfg.ReadEndpoints {
-			epHandlers := cache.ResourceEventHandlerFuncs{
-				AddFunc: func(obj interface{}) {
-					key, err := cache.MetaNamespaceKeyFunc(obj)
-					if err == nil {
-						c.queue.Add(svcKey(key))
-					}
-				},
-				UpdateFunc: func(old interface{}, new interface{}) {
-					key, err := cache.MetaNamespaceKeyFunc(new)
-					if err == nil {
-						c.queue.Add(svcKey(key))
-					}
-				},
-				DeleteFunc: func(obj interface{}) {
-					key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
-					if err == nil {
-						c.queue.Add(svcKey(key))
-					}
-				},
+	svcHandlers := cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			key, err := cache.MetaNamespaceKeyFunc(obj)
+			if err == nil {
+				c.queue.Add(svcKey(key))
 			}
-			epWatcher := cache.NewListWatchFromClient(c.client.CoreV1().RESTClient(), "endpoints", v1.NamespaceAll, fields.Everything())
-			c.epIndexer, c.epInformer = cache.NewIndexerInformer(epWatcher, &v1.Endpoints{}, 0, epHandlers, cache.Indexers{})
-
-			c.syncFuncs = append(c.syncFuncs, c.epInformer.HasSynced)
-		}
+		},
+		UpdateFunc: func(old interface{}, new interface{}) {
+			key, err := cache.MetaNamespaceKeyFunc(new)
+			if err == nil {
+				c.queue.Add(svcKey(key))
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+			if err == nil {
+				c.queue.Add(svcKey(key))
+			}
+		},
 	}
+	svcWatcher := cache.NewListWatchFromClient(c.client.CoreV1().RESTClient(), "services", corev1.NamespaceAll, fields.Everything())
+	c.svcIndexer, c.svcInformer = cache.NewIndexerInformer(svcWatcher, &corev1.Service{}, 0, svcHandlers, cache.Indexers{})
 
-	if cfg.ConfigChanged != nil {
-		cmHandlers := cache.ResourceEventHandlerFuncs{
+	c.serviceChanged = cfg.ServiceChanged
+	c.syncFuncs = append(c.syncFuncs, c.svcInformer.HasSynced)
+
+	// Endpoint Watcher (only used by Nodes, not Allocators)
+
+	if cfg.ReadEndpoints {
+		epHandlers := cache.ResourceEventHandlerFuncs{
 			AddFunc: func(obj interface{}) {
 				key, err := cache.MetaNamespaceKeyFunc(obj)
 				if err == nil {
-					c.queue.Add(cmKey(key))
+					c.queue.Add(svcKey(key))
 				}
 			},
 			UpdateFunc: func(old interface{}, new interface{}) {
 				key, err := cache.MetaNamespaceKeyFunc(new)
 				if err == nil {
-					c.queue.Add(cmKey(key))
+					c.queue.Add(svcKey(key))
 				}
 			},
 			DeleteFunc: func(obj interface{}) {
 				key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
 				if err == nil {
-					c.queue.Add(cmKey(key))
+					c.queue.Add(svcKey(key))
 				}
 			},
 		}
-		cmWatcher := cache.NewListWatchFromClient(c.client.CoreV1().RESTClient(), "configmaps", namespace, fields.OneTermEqualSelector("metadata.name", cfg.ConfigMapName))
-		c.cmIndexer, c.cmInformer = cache.NewIndexerInformer(cmWatcher, &v1.ConfigMap{}, 0, cmHandlers, cache.Indexers{})
+		epWatcher := cache.NewListWatchFromClient(c.client.CoreV1().RESTClient(), "endpoints", corev1.NamespaceAll, fields.Everything())
+		c.epIndexer, c.epInformer = cache.NewIndexerInformer(epWatcher, &corev1.Endpoints{}, 0, epHandlers, cache.Indexers{})
 
-		c.configChanged = cfg.ConfigChanged
-		c.syncFuncs = append(c.syncFuncs, c.cmInformer.HasSynced)
+		c.syncFuncs = append(c.syncFuncs, c.epInformer.HasSynced)
 	}
+
+	// ConfigMap Watcher
+
+	cmHandlers := cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			key, err := cache.MetaNamespaceKeyFunc(obj)
+			if err == nil {
+				c.queue.Add(cmKey(key))
+			}
+		},
+		UpdateFunc: func(old interface{}, new interface{}) {
+			key, err := cache.MetaNamespaceKeyFunc(new)
+			if err == nil {
+				c.queue.Add(cmKey(key))
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+			if err == nil {
+				c.queue.Add(cmKey(key))
+			}
+		},
+	}
+	namespace := cfg.ConfigMapNS
+	if namespace == "" {
+		bs, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
+		if err != nil {
+			return nil, fmt.Errorf("getting namespace from pod service account data: %s", err)
+		}
+		namespace = string(bs)
+	}
+	cmWatcher := cache.NewListWatchFromClient(c.client.CoreV1().RESTClient(), "configmaps", namespace, fields.OneTermEqualSelector("metadata.name", cfg.ConfigMapName))
+	c.cmIndexer, c.cmInformer = cache.NewIndexerInformer(cmWatcher, &corev1.ConfigMap{}, 0, cmHandlers, cache.Indexers{})
+
+	c.configChanged = cfg.ConfigChanged
+	c.syncFuncs = append(c.syncFuncs, c.cmInformer.HasSynced)
+
+	// Node Watcher
 
 	if cfg.NodeChanged != nil {
 		nodeHandlers := cache.ResourceEventHandlerFuncs{
@@ -245,16 +240,17 @@ func New(cfg *Config) (*Client, error) {
 				}
 			},
 		}
-		nodeWatcher := cache.NewListWatchFromClient(c.client.CoreV1().RESTClient(), "nodes", v1.NamespaceAll, fields.OneTermEqualSelector("metadata.name", cfg.NodeName))
-		c.nodeIndexer, c.nodeInformer = cache.NewIndexerInformer(nodeWatcher, &v1.Node{}, 0, nodeHandlers, cache.Indexers{})
+		nodeWatcher := cache.NewListWatchFromClient(c.client.CoreV1().RESTClient(), "nodes", corev1.NamespaceAll, fields.OneTermEqualSelector("metadata.name", cfg.NodeName))
+		c.nodeIndexer, c.nodeInformer = cache.NewIndexerInformer(nodeWatcher, &corev1.Node{}, 0, nodeHandlers, cache.Indexers{})
 
 		c.nodeChanged = cfg.NodeChanged
 		c.syncFuncs = append(c.syncFuncs, c.nodeInformer.HasSynced)
 	}
 
-	if cfg.Synced != nil {
-		c.synced = cfg.Synced
-	}
+	// Sync Watcher
+
+	c.synced = cfg.Synced
+
 	return c, nil
 }
 
@@ -307,6 +303,7 @@ func (c *Client) Run(stopCh <-chan struct{}) error {
 		}
 		updates.Inc()
 		st := c.sync(key)
+		c.logger.Log("sync", key, "result", st)
 		switch st {
 		case SyncStateSuccess:
 			c.queue.Forget(key)
@@ -333,25 +330,25 @@ func (c *Client) ForceSync() {
 // Update writes svc back into the Kubernetes cluster. If successful,
 // the updated Service is returned. Note that changes to svc.Status
 // are not propagated, for that you need to call UpdateStatus.
-func (c *Client) Update(svc *v1.Service) (*v1.Service, error) {
+func (c *Client) Update(svc *corev1.Service) (*corev1.Service, error) {
 	return c.client.CoreV1().Services(svc.Namespace).Update(context.TODO(), svc, metav1.UpdateOptions{})
 }
 
 // UpdateStatus writes the protected "status" field of svc back into
 // the Kubernetes cluster.
-func (c *Client) UpdateStatus(svc *v1.Service) error {
+func (c *Client) UpdateStatus(svc *corev1.Service) error {
 	_, err := c.client.CoreV1().Services(svc.Namespace).UpdateStatus(context.TODO(), svc, metav1.UpdateOptions{})
 	return err
 }
 
 // Infof logs an informational event about svc to the Kubernetes cluster.
-func (c *Client) Infof(svc *v1.Service, kind, msg string, args ...interface{}) {
-	c.events.Eventf(svc, v1.EventTypeNormal, kind, msg, args...)
+func (c *Client) Infof(svc *corev1.Service, kind, msg string, args ...interface{}) {
+	c.events.Eventf(svc, corev1.EventTypeNormal, kind, msg, args...)
 }
 
 // Errorf logs an error event about svc to the Kubernetes cluster.
-func (c *Client) Errorf(svc *v1.Service, kind, msg string, args ...interface{}) {
-	c.events.Eventf(svc, v1.EventTypeWarning, kind, msg, args...)
+func (c *Client) Errorf(svc *corev1.Service, kind, msg string, args ...interface{}) {
+	c.events.Eventf(svc, corev1.EventTypeWarning, kind, msg, args...)
 }
 
 func (c *Client) sync(key interface{}) SyncState {
@@ -369,7 +366,7 @@ func (c *Client) sync(key interface{}) SyncState {
 			return c.serviceChanged(l, string(k), nil, nil)
 		}
 
-		var eps *v1.Endpoints
+		var eps *corev1.Endpoints
 		if c.epIndexer != nil {
 			epsIntf, exists, err := c.epIndexer.GetByKey(string(k))
 			if err != nil {
@@ -379,10 +376,10 @@ func (c *Client) sync(key interface{}) SyncState {
 			if !exists {
 				return c.serviceChanged(l, string(k), nil, nil)
 			}
-			eps = epsIntf.(*v1.Endpoints)
+			eps = epsIntf.(*corev1.Endpoints)
 		}
 
-		return c.serviceChanged(l, string(k), svc.(*v1.Service), eps)
+		return c.serviceChanged(l, string(k), svc.(*corev1.Service), eps)
 
 	case cmKey:
 		l := log.With(c.logger, "configmap", string(k))
@@ -400,7 +397,7 @@ func (c *Client) sync(key interface{}) SyncState {
 		// or validation, result in a "synced" state, because the
 		// config is not going to parse any better until the k8s
 		// object changes to fix the issue.
-		cm := cmi.(*v1.ConfigMap)
+		cm := cmi.(*corev1.ConfigMap)
 		cfg, err := config.Parse([]byte(cm.Data["config"]))
 		if err != nil {
 			l.Log("event", "configStale", "error", err, "msg", "config (re)load failed, config marked stale")
@@ -432,7 +429,7 @@ func (c *Client) sync(key interface{}) SyncState {
 			l.Log("op", "getNode", "error", "node doesn't exist in k8s, but I'm running on it!")
 			return SyncStateError
 		}
-		node := n.(*v1.Node)
+		node := n.(*corev1.Node)
 		return c.nodeChanged(c.logger, node)
 
 	case synced:
