67026722a8c30f66493accf4904c2085fc0d7906
toby cabot <toby@acnodal.io>
2020-09-22 11:00:09 -0400

    Delete unused "labels" code
    We used to pass this data around but now the main function figures out
    the set of nodes and passes it to the election so we don't need this
    code.

--- internal/local/announcer.go
+++ internal/local/announcer.go
@@ -18,7 +18,6 @@ import (
 	"net"
 
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/labels"
 
 	"purelb.io/internal/election"
 	"purelb.io/internal/lbnodeagent"
@@ -31,7 +30,6 @@ import (
 type announcer struct {
 	logger     log.Logger
 	myNode     string
-	nodeLabels labels.Set
 	config     *purelbv1.LBNodeAgentLocalSpec
 	groups     map[string]*purelbv1.ServiceGroupLocalSpec // groupName -> ServiceGroupLocalSpec
 	svcAdvs    map[string]net.IP                          // svcName -> IP
