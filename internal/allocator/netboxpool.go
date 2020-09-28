1d2a1d99398978c1ae3a9607920f3a94b00b2fc5
toby cabot <toby@acnodal.io>
2020-09-28 13:17:46 -0400

    Netbox: first-class configuration support
    We used to support Netbox address allocation as a side-effect of the
    Acnodal Enterprise GateWay service group.  Now we support a
    ServiceGroupNetboxSpec which explicitly configures PureLB to request
    addresses from Netbox.
    
    The spec has three parameters:
    
    * URL: the base URL of the Netbox server
    * Tenant: the "slug" value of the tenant that owns the addresses
    * Aggregation: works the same as local aggregation

--- internal/allocator/netboxpool.go
+++ internal/allocator/netboxpool.go
@@ -25,13 +25,9 @@ import (
 	"purelb.io/internal/netbox"
 )
 
-const (
-	tenant = "ipam-purelb"
-)
-
-// EGWPool represents an IP address pool on the Acnodal Enterprise
-// GateWay.
-type EGWPool struct {
+// NetboxPool is the IP address pool that requests IP addresses from a
+// Netbox IPAM system.
+type NetboxPool struct {
 	url       string
 	userToken string
 	netbox    netbox.Netbox
@@ -48,25 +44,22 @@ type EGWPool struct {
 	aggregation string
 }
 
-// NewEGWPool initializes a new instance of EGWPool. If error is
-// non-nil then the returned EGWPool should not be used.
-func NewEGWPool(rawurl string, aggregation string) (*EGWPool, error) {
-	// Make sure that we've got credentials
+// NewNetboxPool initializes a new instance of NetboxPool. If error is
+// non-nil then the returned NetboxPool should not be used.
+func NewNetboxPool(rawurl string, tenant string, aggregation string) (*NetboxPool, error) {
+	// Make sure that we've got credentials for Netbox
 	userToken, ok := os.LookupEnv("NETBOX_USER_TOKEN")
 	if !ok {
 		return nil, fmt.Errorf("NETBOX_USER_TOKEN not set, can't connect to Netbox")
 	}
 
-	// Use the hostname from the service group, but reset the path.  EGW
-	// and Netbox each have their own API URL schemes so we only need
-	// the protocol, host, port, credentials, etc.
+	// Validate the url from the service group
 	url, err := url.Parse(rawurl)
 	if err != nil {
 		return nil, fmt.Errorf("Netbox URL invalid")
 	}
-	url.Path = ""
 
-	return &EGWPool{
+	return &NetboxPool{
 		url:            rawurl,
 		userToken:      userToken,
 		netbox:         *netbox.NewNetbox(url.String(), tenant, userToken),
@@ -81,7 +74,7 @@ func NewEGWPool(rawurl string, aggregation string) (*EGWPool, error) {
 // depends on whether another service is using the address, and if so,
 // whether this service can share the address with it. error will be
 // nil if the ip is available, and will contain an explanation if not.
-func (p EGWPool) Available(ip net.IP, ports []Port, service string, key *Key) error {
+func (p NetboxPool) Available(ip net.IP, ports []Port, service string, key *Key) error {
 	// No key: no sharing
 	if key == nil {
 		key = &Key{}
@@ -118,11 +111,11 @@ func (p EGWPool) Available(ip net.IP, ports []Port, service string, key *Key) er
 }
 
 // AssignNext assigns a service to the next available IP.
-func (p EGWPool) AssignNext(service string, ports []Port, sharingKey *Key) (net.IP, error) {
+func (p NetboxPool) AssignNext(service string, ports []Port, sharingKey *Key) (net.IP, error) {
 	// fetch from netbox
 	cidr, err := p.netbox.Fetch()
 	if err != nil {
-		return nil, fmt.Errorf("no available IPs in pool")
+		return nil, fmt.Errorf("no available IPs in pool %q", err)
 	}
 	ip, _, err := net.ParseCIDR(cidr)
 	if err != nil {
@@ -133,23 +126,23 @@ func (p EGWPool) AssignNext(service string, ports []Port, sharingKey *Key) (net.
 }
 
 // Assign assigns a service to an IP.
-func (p EGWPool) Assign(ip net.IP, ports []Port, service string, sharingKey *Key) error {
+func (p NetboxPool) Assign(ip net.IP, ports []Port, service string, sharingKey *Key) error {
 	return nil
 }
 
 // Release releases an IP so it can be assigned again.
-func (p EGWPool) Release(ip net.IP, service string) {
+func (p NetboxPool) Release(ip net.IP, service string) {
 }
 
 // InUse returns the count of addresses that currently have services
 // assigned.
-func (p EGWPool) InUse() int {
+func (p NetboxPool) InUse() int {
 	return -1
 }
 
 // servicesOnIP returns the names of the services who are assigned to
 // the address.
-func (p EGWPool) servicesOnIP(ip net.IP) []string {
+func (p NetboxPool) servicesOnIP(ip net.IP) []string {
 	ipstr := ip.String()
 	svcs, has := p.addressesInUse[ipstr]
 	if has {
@@ -163,37 +156,37 @@ func (p EGWPool) servicesOnIP(ip net.IP) []string {
 }
 
 // SharingKey returns the "sharing key" for the specified address.
-func (p EGWPool) SharingKey(ip net.IP) *Key {
+func (p NetboxPool) SharingKey(ip net.IP) *Key {
 	return p.sharingKeys[ip.String()]
 }
 
 // First returns the first (i.e., lowest-valued) net.IP within this
 // Pool, or nil if the pool has no addresses.
-func (p EGWPool) First() net.IP {
+func (p NetboxPool) First() net.IP {
 	return nil
 }
 
 // Next returns the next net.IP within this Pool, or nil if the
 // provided net.IP is the last address in the range.
-func (p EGWPool) Next(ip net.IP) net.IP {
+func (p NetboxPool) Next(ip net.IP) net.IP {
 	return nil
 }
 
 // Size returns the total number of addresses in this pool if it's a
 // local pool, or 0 if it's a remote pool.
-func (p EGWPool) Size() uint64 {
+func (p NetboxPool) Size() uint64 {
 	return uint64(0)
 }
 
 // Overlaps indicates whether the other Pool overlaps with this one
 // (i.e., has any addresses in common).  It returns true if there are
 // any common addresses and false if there aren't.
-func (p EGWPool) Overlaps(other Pool) bool {
+func (p NetboxPool) Overlaps(other Pool) bool {
 	return false
 }
 
 // Contains indicates whether the provided net.IP represents an
 // address within this Pool.  It returns true if so, false otherwise.
-func (p EGWPool) Contains(ip net.IP) bool {
+func (p NetboxPool) Contains(ip net.IP) bool {
 	return false
 }
