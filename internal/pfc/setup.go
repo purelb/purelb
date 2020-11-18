20c0145c68c1715e9eb4cb42662b371167aeeef4
toby cabot <toby@acnodal.io>
2020-11-18 14:19:59 -0500

    Fix cli_cfg configuration check
    We were looking for "GUE_DECAP" but that doesn't appear in all valid
    cases so now we look for the interface name which will always be
    present in the output if the interface is configured properly.

--- internal/pfc/setup.go
+++ internal/pfc/setup.go
@@ -54,6 +54,6 @@ func addFilter(nic string, direction string) error {
 
 func configurePFC(nic string, qid int, flags int) error {
 	// configure the PFC only if it hasn't been already
-	cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("/opt/acnodal/bin/cli_cfg get %[1]s | grep GUE-DECAP || /opt/acnodal/bin/cli_cfg set %[1]s %[2]d 0 %[3]d \"%[1]s rx\"", nic, qid, flags))
+	cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("/opt/acnodal/bin/cli_cfg get %[1]s | grep %[1]s || /opt/acnodal/bin/cli_cfg set %[1]s %[2]d 0 %[3]d \"%[1]s rx\"", nic, qid, flags))
 	return cmd.Run()
 }
