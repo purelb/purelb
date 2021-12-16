b24c920bd990c599402cd8cfd86cad3836dbf0ee
toby cabot <toby@acnodal.io>
2021-12-16 16:42:05 -0500

    Fix servicegroup "successful parse" message
    We had an extra parameter in the call.

--- internal/allocator/allocator.go
+++ internal/allocator/allocator.go
@@ -252,7 +252,7 @@ func (a *Allocator) parseConfig(groups []*purelbv1.ServiceGroup) (map[string]Poo
 		}
 
 		pools[group.Name] = pool
-		a.client.Infof(group, "Parsed", "ServiceGroup parsed successfully", group.Name)
+		a.client.Infof(group, "Parsed", "ServiceGroup parsed successfully")
 	}
 
 	return pools, nil
