b7f9fb44967529efefa15753823f331eb2e9a5b8
Jayadeep KM <kmjayadeep@gmail.com>
2022-04-29 19:04:06 +0200

    fix ipv6 not allocating address from specified loadbalancerIP
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- internal/allocator/localpool.go
+++ internal/allocator/localpool.go
@@ -350,8 +350,8 @@ func (p LocalPool) Overlaps(other Pool) bool {
 // Contains indicates whether the provided net.IP represents an
 // address within this Pool.  It returns true if so, false otherwise.
 func (p LocalPool) Contains(ip net.IP) bool {
-	if p.v4Range != nil {
-		return p.v4Range.Contains(ip)
+	if p.v4Range != nil && ip.To4() != nil {
+		return p.v4Range.Contains(ip.To4())
 	}
 	if p.v6Range != nil {
 		return p.v6Range.Contains(ip)
